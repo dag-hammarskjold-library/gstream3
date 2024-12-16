ecb97bd7a46842f2fde45646b7d620074a145930
Aaron Helton <helton@un.org>
2024-12-16 13:04:16 -0500

    alter naming scheme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,12 +4,8 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['dayjs-nuxt', '@nuxtjs/tailwindcss'],
   runtimeConfig: {
-    //AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
-    //AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
-    //REGION: process.env.REGION,
     public: {
-      // Override this with the environment variable NUXT_PUBLIC_GDOC_ENV
-      GDOC_ENV: process.env.NUXT_PUBLIC_GDOC_ENV || 'prod'
+      GDOC_ENV: process.env.GDOC_ENV || 'prod'
     },
   },
   app: {
