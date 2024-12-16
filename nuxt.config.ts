b034e21aca40130732456e4b6fea2ddfa248f346
Aaron Helton <helton@un.org>
2024-12-16 13:46:24 -0500

    get secrets using backend tools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,14 @@
+import { secret } from '@aws-amplify/backend'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['dayjs-nuxt', '@nuxtjs/tailwindcss'],
   runtimeConfig: {
-    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID || 'none',
-    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY || 'none',
-    AWS_DEFAULT_REGION: process.env.AWS_DEFAULT_REGION || 'us-east-1',
+    AWS_ACCESS_KEY_ID: secret('AWS_ACCESS_KEY_ID') || 'none',
+    AWS_SECRET_ACCESS_KEY: secret('AWS_SECRET_ACCESS_KEY') || 'none',
+    AWS_DEFAULT_REGION: secret('AWS_DEFAULT_REGION') || 'us-east-1',
     public: {
       GDOC_ENV: process.env.GDOC_ENV || 'prod'
     },
