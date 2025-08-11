3deab237ea6ef89f2b68999ff204634bc9be49c6
Aaron Helton <helton@un.org>
2025-08-11 13:16:52 -0400

    tweak test environment

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,11 @@
 import { defineVitestConfig } from '@nuxt/test-utils/config'
+import { loadEnv } from 'vite'
 
 export default defineVitestConfig({
   test: {
+    root: process.cwd(),
+    // https://github.com/vitest-dev/vitest/issues/2117#issuecomment-1890908753
+    env: loadEnv('', process.cwd(), ''),
     globals: true,
     environmentOptions: {
       nuxt: {
