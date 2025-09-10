868d1f96a00a530fc643760c83a3688aa263b74c
Aaron Helton <helton@un.org>
2025-09-10 10:59:35 -0400

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
     AwsAcessKeyId: process.env.AWS_ACCESS_KEY_ID || 'none',
     AwsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || 'none',
     public: {
-      version: "v0.1.12",
+      version: "v0.1.14",
       gdocEnv: process.env.GDOC_ENV || 'prod'
     },
   },
